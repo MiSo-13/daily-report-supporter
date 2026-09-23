6c286e1a660c46da2f3157864c8af1bc9a2211fd
Jh_MiSo <72727535+MiSo-13@users.noreply.github.com>
2026-09-23 15:23:15 +0900

    fix: configure xcb before importing PyQt

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-import os
-import sys
+from app.qt_platform import configure_qt_platform
 
-
-def _configure_qt_platform() -> None:
-    if not sys.platform.startswith("linux"):
-        return
-    if os.environ.get("QT_QPA_PLATFORM"):
-        return
-
-    # 일부 Linux/Crostini/Wayland 환경에서 QMenu/QDialog popup surface를
-    # 닫을 때 Qt Wayland connection이 끊기는 문제가 있다.
-    # X11 display도 제공되는 환경에서는 Qt가 시작되기 전에 xcb를 사용한다.
-    if os.environ.get("WAYLAND_DISPLAY") and os.environ.get("DISPLAY"):
-        os.environ["QT_QPA_PLATFORM"] = "xcb"
-
-
-_configure_qt_platform()
+configure_qt_platform()
 
 from app.main_window import run
 
